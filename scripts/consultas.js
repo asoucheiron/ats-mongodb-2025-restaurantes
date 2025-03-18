e96651d5669a5b1b8056974649994e458bb896b1
asoucheiron <1635742@uab.cat>
2025-03-18 20:11:31 +0100

    arxiu consultes fetes

--- scripts/consultas.js
+++ scripts/consultas.js
@@ -1,1 +1,140 @@
-//Inici git
+//TAREAS OBLIGATORIAS
+
+//2.Implementación de consultas en MongoDB
+
+/*
+Buscar todos los restaurantes de un tipo de comida específico (ej. "Chinese").
+*/
+db.restaurants.find(
+    { "type_of_food": "Curry" },
+    { "_id": 0, "name": 1, "type_of_food": 1 }
+)
+
+/*
+Listar las inspecciones con violaciones, ordenadas por fecha.
+*/
+db.inspections.find(
+    { result: "Violation Issued" },
+    { _id: 1, restaurant_id: 1, date: 1, result: 1 })
+    .sort({ date: 1 })
+
+/*
+Encontrar restaurantes con una calificación superior a 4.
+*/
+db.restaurants.find(
+    { "rating": { $gt: 4 } },
+    { "_id": 0, "name": 1, "rating": 1 }
+)
+
+
+//Uso de Agregaciones
+
+/*
+Agrupar restaurantes por tipo de comida y calcular la calificación promedio.
+*/
+db.restaurants.aggregate([
+    {
+        $group: {
+            _id: "$type_of_food", avg_rating: { $avg: "$rating" }
+        }
+    }])
+
+/*
+Contar el número de inspecciones por resultado y mostrar los porcentajes.
+*/
+db.inspections.aggregate([
+    {
+        $group: {
+            _id: "$result",
+            count: { $sum: 1 }
+        }
+    },
+    {
+        $group: {
+            _id: null,
+            totalInspections: { $sum: "$count" },
+            results: { $push: { result: "$_id", count: "$count" } }
+        }
+    },
+    {
+        $unwind: "$results"
+    },
+    {
+        $project: {
+            _id: 0,
+            result: "$results.result",
+            count: "$results.count",
+            percentage: {
+                $multiply: [{ $divide: ["$results.count", "$totalInspections"] }, 100]
+            }
+        }
+    }
+])
+
+
+/*
+Unir restaurantes con sus inspecciones utilizando $lookup.
+*/
+db.restaurants.aggregate([{
+    "$lookup": {
+        "from": "inspections",
+        "let": {
+            "restaurantId": "$_id"
+        },
+        "pipeline": [
+            {
+                "$match": {
+                    "$expr": {
+                        "$eq": [
+                            {
+                                "$toString": "$restaurant_id"
+                            },
+                            {
+                                "$toString": "$$restaurantId"
+                            }
+                        ]
+                    }
+                }
+            }
+        ],
+        "as": "restaurantInspections"
+    }
+}
+]
+)
+
+//TAREAS AVANZADAS
+
+//1. Optimización del rendimiento
+
+/*
+Implementar índices adecuados para las consultas
+*/
+db.inspections.createIndex({ "restaurant_id": 1, "result": 1 })
+
+db.restaurants.createIndex({ "address line 2": 1, "type_of_food": 1 })
+
+/*
+Comparar el rendimiento antes y después de crear los índices utilizando explain()
+*/
+db.inspections.find({
+    "restaurant_id": "55f14312c7447c3da7051b26",
+    "result": "Pass"
+}).explain("executionStats")
+
+
+db.restaurants.find(
+    {
+        "type_of_food": "Chinese",
+        "address line 2": "London",
+        "rating": { $gt: 4 }
+    }
+).explain("executionStats")
+
+
+
+
+
+
+
+
